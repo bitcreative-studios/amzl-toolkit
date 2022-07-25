3df59eadd0eb794b67d06a0a7a0651947bf47b44
George Bockari <bitwhys@gmail.com>
2022-07-25 14:47:00 -0400

    feat: set up packages with deps

---
+++ packages/addon/src/Sidebar/SidebarPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export type SidebarPanelProps = {}
+
+const SidebarPanel = ({}: SidebarPanelProps) => {
+  return <div>This is the sidebar</div>
+}
+
+export default SidebarPanel
